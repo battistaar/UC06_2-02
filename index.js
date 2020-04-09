77869d9102e85fe09b1b586d9aea44bb0b0e0390
Enrico Battistella <enrico.battistella@digitalrockers.it>
2020-04-09 08:23:35 +0200

    basic express

--- index.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello world');
-}).listen(3000, () => console.log('Server started on port 3000'));
